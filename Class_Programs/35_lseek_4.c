253070165f224c1bd9f4a03bf2bff4bb8581ca18
new name <pravin.p.lanjekar@gmail.com>
2026-04-04 19:57:45 +0530

    Add files via upload

---
+++ Class_Programs/35_lseek_4.c
@@ -0,0 +1,39 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<fcntl.h>
+#include<string.h>
+#include<errno.h>
+
+#define SIZE_BUFFER 100
+
+int main()
+{
+    int fd = 0;
+    char Buffer[SIZE_BUFFER];
+    int iRet = 0;
+
+    memset(Buffer,'\0',SIZE_BUFFER);
+
+    fd = open("Hole.txt",O_WRONLY | O_CREAT);
+    
+    if(fd < 0)
+    {
+        printf("Unable to open the file\n");
+        printf("Reson : %s\n",strerror(errno));
+        return -1;
+    }
+
+    printf("File succesfully opened with fd : %d\n",fd);
+
+    iRet = lseek(fd,4100,SEEK_SET);
+
+    printf("Current offset is : %d\n",iRet);
+
+    iRet = write(fd,"END",3);
+
+    printf("%d bytes gets written succesfully\n",iRet);
+    
+    close(fd);
+    
+    return 0;
+}
